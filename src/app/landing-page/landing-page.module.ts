9d4e09b8c92cce1be45085cf4773461f73979531
Bailey <bailey.pearson@vanderbilt.edu>
2020-03-31 20:15:10 -0400

    Add landing page and ngswitch

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
 
 
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, LoginComponentComponent, RegisterComponentComponent],
   imports: [
     CommonModule
   ]
--- src/app/landing-page/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page/landing-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { landingPageRoute } from 'src/app/shared/constants/routing-constants';
+
+type LandingPageState = 'login' | 'register';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
+  landingPageState: LandingPageState = 'login';
 
   constructor() { }
 
-  ngOnInit(): void {
+  toggleState() {
+    this.landingPageState = this.landingPageState === 'login' ? 'register' : 'login';
+    console.log(this.landingPageState);
   }
-
 }
